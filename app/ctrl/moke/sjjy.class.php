7628cb3487628e2a9666512f8d34a8fbfa43ca01
RajeshZQZ <939546484@qq.com>
2018-08-30 02:58:29 +0800

    err

--- app/ctrl/moke/sjjy.class.php
+++ app/ctrl/moke/sjjy.class.php
@@ -3,14 +3,18 @@
 class ctrl_moke_sjjy {
   public static $info = array();
     function test01 (){
-
-        $infoArr = array(
-            'name' => $_REQUEST['name'],
-            'age' => $_REQUEST['age'],
-            'sex' => $_REQUEST['sex'],
+        $infoArr = array();
+        if (empty($_REQUEST['name']) && empty($_REQUEST['age'] && empty($_REQUEST['sex'] ){
+        //参数都为空
+        }else {
+            $infoArr = array(
+                'name' => $_REQUEST['name'],
+                'age' => $_REQUEST['age'],
+                'sex' => $_REQUEST['sex'],
             );
+        }
         self::$info = $infoArr;
-    $this->dotest($infoArr);
+        $this->dotest($infoArr);
     }
 
     function dotest($info){
