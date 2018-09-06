4de9704020e80abd81a846d7f0d4a478f1a229d9
RajeshZQZ <939546484@qq.com>
2018-09-06 15:28:22 +0800

    config

--- app/config.php
+++ app/config.php
@@ -9,6 +9,6 @@
 //define ('PATTH_FRAMEWORK',dirname(__FILE__) . '/framework');
 //define ('ACT_PATH',dirname(__FILE__) . '/ctrl/');
 
-define ('APP_DIR','/usr/local/httpd/htdocs/game01/wap/app/');
+define ('APP_DIR',dirname(__FILE__).'/');
 define('TEMPLATE','/usr/local/httpd/htdocs/game01/wap/app/template/');
 define ('URL','http://47.98.188.59/game01/wap/');
